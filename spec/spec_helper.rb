bd81e3d45e8f5e9e202821156a219a6931896364
Ashley Abbott <a.j.abbott24@gmail.com>
2021-10-03 16:46:10 +0100

    using wear_merged! in place of wear!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'coveralls'
 
-Coveralls.wear! unless ENV['CI'].nil?
+Coveralls.wear_merged! unless ENV['CI'].nil?
+
+if ENV['SIMPLECOV'] == '1'
+  require 'simplecov'
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+end
