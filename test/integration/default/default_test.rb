5e1adca262f824a073c9f6bd243ed7b16f5bc98c
Ashley Abbott <ash.abbott@appsbroker.com>
2022-12-20 17:16:53 +0000

    require libs

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,14 +1,11 @@
+require_relative '../../../libraries/consul_template_helpers'
+
 # InSpec test for recipe consul_template::default
 
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
 unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-
   describe file("#{Chef::Config['file_cache_path']}/#{consul_template.archive}") do
     it { should exist }
     its('sha256') { should eq consul_template.checksum }
